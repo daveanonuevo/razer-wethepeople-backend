16530f8eef948d05572ccbfd5c7a228248735372
Lian Exelbert Anier Tan <53962638+ExelTan@users.noreply.github.com>
2020-05-15 15:58:43 +0800

    Made Smoll changes
    Update Created New Files

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const moneyRouter = require('./money');
+const activitiesRouter=require('./activities');
 
 router.use('/money', moneyRouter);
-
+router.use('/activities',activitiesRouter);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("/ endpoint");
