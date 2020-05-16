68f3242d2283feb14891f231dfe94ca1c52c1818
Lian Exelbert Anier Tan <53962638+ExelTan@users.noreply.github.com>
2020-05-17 00:44:03 +0800

    Done

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,13 @@ const servicesRouter=require('./services');
 
 router.use('/money', moneyRouter);
 router.use('/services',servicesRouter);
+const foodRouter = require('./food');
+const servicesRouter = require('./services');
+const trasnportRouter = require('/transport');
+router.use('/money', moneyRouter);
+router.use('/food', foodRouter);
+router.use('/services', servicesRouter);
+router.use('/transport', trasnportRouter);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("/ endpoint");
